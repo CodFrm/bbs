c4b5a6a97e406e715dc9e82af83ea91aac3ab93e
CodFrm <code.farmer@qq.com>
2018-02-23 09:28:30 +0800

    db

--- app/install/bbs.sql
+++ app/install/bbs.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-01-20 14:36:45
+Date: 2018-02-23 09:27:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,16 +20,25 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `bbs_area`;
 CREATE TABLE `bbs_area` (
-  `aid` int(11) NOT NULL,
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `remake` varchar(255) NOT NULL,
   `father_aid` int(11) NOT NULL,
   PRIMARY KEY (`aid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of bbs_area
 -- ----------------------------
+INSERT INTO `bbs_area` VALUES ('1', '会员互动', '会员互动的区域', '0');
+INSERT INTO `bbs_area` VALUES ('2', '娱乐闲聊', '', '1');
+INSERT INTO `bbs_area` VALUES ('3', '日常灌水', '', '1');
+INSERT INTO `bbs_area` VALUES ('4', '资源分享', '', '0');
+INSERT INTO `bbs_area` VALUES ('5', '音乐', '', '4');
+INSERT INTO `bbs_area` VALUES ('6', '游戏', '', '4');
+INSERT INTO `bbs_area` VALUES ('7', '软件', '', '4');
+INSERT INTO `bbs_area` VALUES ('8', '电影/视频', '', '4');
+INSERT INTO `bbs_area` VALUES ('9', '校园生活', '', '1');
 
 -- ----------------------------
 -- Table structure for bbs_auth
@@ -40,13 +49,14 @@ CREATE TABLE `bbs_auth` (
   `auth_interface` varchar(255) NOT NULL COMMENT '权限接口',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`auth_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of bbs_auth
 -- ----------------------------
 INSERT INTO `bbs_auth` VALUES ('1', 'admin', '管理员页面权限');
-INSERT INTO `bbs_auth` VALUES ('2', 'index', '首页浏览');
+INSERT INTO `bbs_auth` VALUES ('2', 'index->index', '首页浏览');
+INSERT INTO `bbs_auth` VALUES ('3', 'index->area', '发帖权限');
 
 -- ----------------------------
 -- Table structure for bbs_config
@@ -102,6 +112,7 @@ CREATE TABLE `bbs_groupauth` (
 -- ----------------------------
 INSERT INTO `bbs_groupauth` VALUES ('1', '1');
 INSERT INTO `bbs_groupauth` VALUES ('2', '2');
+INSERT INTO `bbs_groupauth` VALUES ('3', '3');
 
 -- ----------------------------
 -- Table structure for bbs_posts
@@ -112,15 +123,17 @@ CREATE TABLE `bbs_posts` (
   `uid` bigint(20) NOT NULL,
   `title` varchar(255) NOT NULL,
   `mkdown` text NOT NULL,
-  `html` mediumblob NOT NULL,
+  `html` mediumtext NOT NULL,
   `time` bigint(20) NOT NULL,
   `aid` int(11) NOT NULL,
   PRIMARY KEY (`tid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of bbs_posts
 -- ----------------------------
+INSERT INTO `bbs_posts` VALUES ('1', '1', 'asdasfsafsdfsgwreg', '# test\r\nasfsfsd\r\nd\r\nfd\r\nf\r\ndf\r\nvd\r\nfb\r\ndf\r\nbdfbwer\r\n', '<h1>test</h1>\n\n<p>asfsfsd\nd\nfd\nf\ndf\nvd\nfb\ndf\nbdfbwer</p>\n', '1516600285', '2');
+INSERT INTO `bbs_posts` VALUES ('2', '1', '发表文章', '# test\r\n\r\n家哈哈四的\r\n\r\n\r\n么啊大神的v覆盖34人个发表', '<h1>test</h1>\n\n<p>家哈哈四的</p>\n\n<p>么啊大神的v覆盖34人个发表</p>\n', '1517393697', '7');
 
 -- ----------------------------
 -- Table structure for bbs_token
@@ -137,7 +150,10 @@ CREATE TABLE `bbs_token` (
 -- ----------------------------
 -- Records of bbs_token
 -- ----------------------------
+INSERT INTO `bbs_token` VALUES ('FWRBhChesdRXIkOBgr0s4KHCqYuNG6dsP1Mc6I2cz5t2qIjDANO6xvgJ3iw01eZr', '958139621@qq.com', '1516590244', '1');
 INSERT INTO `bbs_token` VALUES ('KpFtnAt1ln66BlFd', '1', '1516084441', '0');
+INSERT INTO `bbs_token` VALUES ('rbqbBlxq8u5DriU5', '1', '1516512579', '0');
+INSERT INTO `bbs_token` VALUES ('UyICZpTxTbur8sIJ', '1', '1517393650', '0');
 
 -- ----------------------------
 -- Table structure for bbs_usergroup
